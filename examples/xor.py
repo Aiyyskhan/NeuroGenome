10d79092a49170077162668fbe59ebfc295dfc33
aiyyskhan <aiyyskhan@gmail.com>
2022-04-10 10:56:47 +0900

    added a variant with the ReLU activation function to xor.py

--- examples/xor.py
+++ examples/xor.py
@@ -40,22 +40,28 @@ SETTINGS = {
 	"schema": schema_0,
 }
 
-VAL_SEQ = [-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0]
+# VAL_SEQ = [-1.0,-0.75,-0.5,-0.25,0.0,0.25,0.5,0.75,1.0]
+# VAL_SEQ = [-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0]
 # VAL_SEQ = [-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0]
+VAL_SEQ = [-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0]
 
 MAX_EPOCH = 500
-# SUCCESS = 0.001
+SUCCESS = 0.001
 
 genome = ng.builders.genome_builder(SETTINGS, VAL_SEQ)
 
 w_matrix_list = ng.builders.neuro_builder(genome)
 
-def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+# def sigmoid(x):
+# 	return 1 / (1 + np.exp(-x))
+
+def relu(x):
+	return np.maximum(x, 0)
 
 def nn(data, wMs):
 	for wM in wMs:
-		data = sigmoid(np.matmul(data, wM))
+		# data = sigmoid(np.matmul(data, wM))
+		data = np.tanh(relu(np.matmul(data, wM)))
 	return data
 
 for gen in range(MAX_EPOCH):
@@ -69,8 +75,8 @@ for gen in range(MAX_EPOCH):
 	if gen % 10 == 0:
 		print(f"Gen: {gen} - Best result {err_arr.min()}")
 
-	# if err_arr.min() <= SUCCESS:
-	# 	break
+	if err_arr.min() <= SUCCESS:
+		break
 
 	lead_genome = ng.evolution.selection(genome, err_arr)
 	genome = ng.evolution.crossover(lead_genome)
